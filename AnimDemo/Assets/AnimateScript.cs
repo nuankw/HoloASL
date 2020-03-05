7e4098b065f55a657451b419a1e16236fca6a2a1
Jix <jitx@users.noreply.github.com>
2020-03-05 01:49:07 -0800

    Feature Update v0.8
    - Added additional animation to avatar
    - Added additional animation to animation controller
    - Changed avatar name
    - Changed animation script name

---
+++ AnimDemo/Assets/AnimateScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateScript : MonoBehaviour {
+
+    public Animator Avatar;
+
+	// Use this for initialization
+	void Start () {
+        Avatar = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Anim_A started.");
+            Avatar.Play("Anim_A");
+            Debug.Log("Anim_A stopped.");
+        }
+        if (Input.GetKeyDown("c"))
+        {
+            Debug.Log("Anim_Hi started.");
+            Avatar.Play("Anim_Hi");
+            Debug.Log("Anim_Hi stopped.");
+        }
+	}
+}
